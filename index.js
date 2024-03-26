6c60c9a5ad930a3fbf8b9ad08773c94414719af0
farzanasrahman <farzana.sadia01@northsouth.edu>
2024-03-26 17:01:26 +0600

    fix: import issue

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 import app from './src/app.js';
 
-app.listen(3000);
+app.listen(3000, () => {
+  console.log('Listening on port 3000');
+});
--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import statRouter from './src/stat/statRouter.js';
+import statRouter from './stat/statRouter.js';
 
 const app = express();
 app.use('/stat', statRouter);
