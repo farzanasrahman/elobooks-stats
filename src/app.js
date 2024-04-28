fe987aed6aef7cc89e588cd3e0e0db6dc9ab80a8
farzanasrahman <farzana.sadia01@northsouth.edu>
2024-04-29 00:46:38 +0600

    feat: error handler

--- src/app.js
+++ src/app.js
@@ -2,8 +2,26 @@ import express from 'express';
 import statRouter from './stat/statRouter.js';
 const app = express();
 
-app.get('/ping', (req, res) => {
-  res.status(200).send('Pong');
+app.get('/ping', (req, res, next) => {
+  try {
+    res.status(200).send('Pong');
+  } catch (error) {
+    next(error);
+  }
 });
+
 app.use('/stat', statRouter);
+
+app.use((req, res, next) => {
+  const error = new Error('Not Found');
+  error.status = 404;
+  next(error);
+});
+
+// eslint-disable-next-line no-unused-vars
+app.use((error, req, res, _) => {
+  error.status = error.status || 500;
+  res.status(error.status);
+  res.send(`${error.status} error, path not found!`);
+});
 export default app;
